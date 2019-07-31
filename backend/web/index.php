092af283e053689e74cf792e221a2285deb2817b
DESKTOP-56SB0F0\miku <404669366@qq.com>
2019-07-31 19:17:40 +0800

    no message

--- backend/web/index.php
+++ backend/web/index.php
@@ -3,5 +3,5 @@ require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
-var_dump($config);exit();
+
 (new yii\web\Application($config))->run();

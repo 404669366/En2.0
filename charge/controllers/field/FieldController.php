df4fd9cd58a80a1259148ec692abd80bdb2824b7
DESKTOP-56SB0F0\miku <404669366@qq.com>
2019-08-29 13:14:02 +0800

    no message

--- charge/controllers/field/FieldController.php
+++ charge/controllers/field/FieldController.php
@@ -10,16 +10,12 @@ namespace app\controllers\field;
 
 
 use app\controllers\basis\AuthController;
+use vendor\project\helpers\Wechat;
 
 class FieldController extends AuthController
 {
     public function actionMap()
     {
-        return $this->render('map.html');
-    }
-
-    public function actionList()
-    {
-        return $this->render('list.html');
+        return $this->render('map.html', ['jsApi' => Wechat::getJsApiParams()]);
     }
 }

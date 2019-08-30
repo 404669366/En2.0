3ea3249578ddc1bdfc672744f272fd494b407013
DESKTOP-56SB0F0\miku <404669366@qq.com>
2019-08-30 13:05:33 +0800

    no message

--- charge/web/js/common.js
+++ charge/web/js/common.js
@@ -115,6 +115,18 @@ window.html = function (inner_id, template_id, data) {
     $('#' + inner_id).html(doT.template($('#' + template_id).text())(data));
 };
 
+window.distance = function (lat1, lng1, lat2, lng2) {
+    var radLat1 = lat1 * Math.PI / 180.0;
+    var radLat2 = lat2 * Math.PI / 180.0;
+    var a = radLat1 - radLat2;
+    var b = lng1 * Math.PI / 180.0 - lng2 * Math.PI / 180.0;
+    var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) +
+        Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
+    s = s * 6378.137;// EARTH_RADIUS;
+    s = Math.round(s * 10000) / 10000;
+    return s;
+};
+
 window.wait = function () {
     var str = '<div class="waitBox"><span><img src="/img/loading.gif" alt=""></span></div>';
     return {

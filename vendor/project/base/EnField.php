f0b683e01f4967cce2e544f9bce835372e149cd7
DESKTOP-56SB0F0\miku <404669366@qq.com>
2019-10-12 11:12:58 +0800

    no message

--- vendor/project/base/EnField.php
+++ vendor/project/base/EnField.php
@@ -558,7 +558,7 @@ class EnField extends \yii\db\ActiveRecord
     public static function getMapInfo($no = '')
     {
         $model = EnOrder::find()->alias('o')
-            ->leftJoin(EnPile::tableName() . ' p', 'p.no=o.pile')
+            ->leftJoin(EnPile::tableName() . ' p', 'p.`no`=o.pile')
             ->leftJoin(EnField::tableName() . ' f', 'f.id=p.field_id')
             ->where(['f.no' => $no]);
         $month = ['-01', '-02', '-03', '-04', '-05', '-06', '-07', '-08', '-09', '-10', '-11', '-12'];

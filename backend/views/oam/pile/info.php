cb11c30b6ae221b8fe0c14965529d2e1c7b6c5f6
DESKTOP-56SB0F0\miku <404669366@qq.com>
2019-10-18 16:11:02 +0800

    no message

--- backend/views/oam/pile/info.php
+++ backend/views/oam/pile/info.php
@@ -169,7 +169,7 @@
                     str += '</td>';
                     str += '<td>' + link[v.linkStatus] + '</td>';
                     str += '<td>' + work[v.workStatus] + '</td>';
-                    if (v.workStatus === 2 && v.linkStatus) {
+                    if (v.workStatus === 2 && v.linkStatus !== 0) {
                         str += '<td><button type="button" class="btn btn-sm btn-danger endCharge" data-no="' + no + '" data-gun="' + k + '">结束充电</button></td>';
                     } else {
                         str += '<td>----</td>';

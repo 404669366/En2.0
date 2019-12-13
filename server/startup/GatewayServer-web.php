f850e1812cf997a4ad5e6c8ffe8ed79a35786002
DESKTOP-56SB0F0\miku <404669366@qq.com>
2019-12-13 10:57:24 +0800

    no message

--- server/startup/GatewayServer-web.php
+++ server/startup/GatewayServer-web.php
@@ -3,5 +3,5 @@ $gateway = new \GatewayWorker\Gateway("websocket://0.0.0.0:20001");
 $gateway->name = 'GatewayServer-web';
 $gateway->registerAddress = '127.0.0.1:20000';
 $gateway->lanIp = '47.99.36.149';
-$gateway->startPort = 22000;
+$gateway->startPort = 4000;
 $gateway->count = 1;

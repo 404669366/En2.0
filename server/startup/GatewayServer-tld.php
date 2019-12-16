797177545116e05af21a3e2010a85c47edb673cd
DESKTOP-56SB0F0\miku <404669366@qq.com>
2019-12-16 14:38:13 +0800

    no message

--- server/startup/GatewayServer-tld.php
+++ server/startup/GatewayServer-tld.php
@@ -1,8 +1,7 @@
 <?php
 $gateway = new \GatewayWorker\Gateway("Tld://0.0.0.0:20002");
 $gateway->name = 'GatewayServer-tld';
-$gateway->lanIp = '172.16.161.173';
-$gateway->registerAddress = '172.16.161.173:20000';
+$gateway->registerAddress = '127.0.0.1:20000';
 $gateway->startPort = 20050;
 $gateway->count = 1;
 $gateway->pingInterval = 30;

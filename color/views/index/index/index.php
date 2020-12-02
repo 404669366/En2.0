008ba16038dc570dddf44145bf0ac20175834782
DESKTOP-OH3AP5P\40466 <404669366@qq.com>
2020-12-03 00:59:30 +0800

    no message

--- color/views/index/index/index.php
+++ color/views/index/index/index.php
@@ -69,7 +69,6 @@
                 change(3, val);
             }
         });
-
         function change(key, val) {
             ratios[key] = val / 100;
             var color = face.mix(ratios).mixToCMYK().toRGB();
